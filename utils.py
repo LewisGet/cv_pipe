edfb1b8ee3d23a82bcfa3b9c0f64daac8b7c3688
Lewis Jang <lewis.aedes@gmail.com>
2025-10-02 16:47:15 +0800

    make until model using lazy load

--- utils.py
+++ utils.py
@@ -205,19 +205,23 @@ def split_long_wav(path):
 
         split_parts += 1
 
+# lazy load
+multiple_speaker_diarization_model_pipeline = None
+
 def get_speaker_dict(path, return_without_save=False):
-    from pyannote.audio import Pipeline
+    if multiple_speaker_diarization_model_pipeline == None:
+        from pyannote.audio import Pipeline
 
-    if config.multiple_speaker_diarization_model.startswith("pyannote"):
-        Pipeline.from_pretrained(config.multiple_speaker_diarization_model, token=config.hugging_face_token)
-    else:
-        Pipeline.from_pretrained(config.multiple_speaker_diarization_model)
+        if config.multiple_speaker_diarization_model.startswith("pyannote"):
+            multiple_speaker_diarization_model_pipeline.from_pretrained(config.multiple_speaker_diarization_model, token=config.hugging_face_token)
+        else:
+            multiple_speaker_diarization_model_pipeline.from_pretrained(config.multiple_speaker_diarization_model)
 
-    pipeline = pipeline.to(torch.device("cuda"))
+        multiple_speaker_diarization_model_pipeline = multiple_speaker_diarization_model_pipeline.to(torch.device("cuda"))
 
     filename = os.path.basename(path)
     waveform, sample_rate = torchaudio.load(path)
-    diarization = pipeline({"waveform": waveform, "sample_rate": sample_rate})
+    diarization = multiple_speaker_diarization_model_pipeline({"waveform": waveform, "sample_rate": sample_rate})
 
     split_datas = []
     for turn, _, speaker in diarization.speaker_diarization.itertracks(yield_label=True):
