e1941de647ad5f65c26b77515b7c5f72a8cc3a9a
Lewis Jang <lewis.aedes@gmail.com>
2025-10-02 16:17:36 +0800

    add analysis speaker function

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ channels = 1
 sample_width = 2
 audio_float_size = 8 * sample_width
 max_audio_length_ms = 1000 * 60 * 15
+min_audio_length_ms = 1000
 
 raw_audio_path = create_directory(".", "raw")
 train_format_audio_path = create_directory(".", "format")
@@ -25,9 +26,12 @@ weight_and_bias_api = ""
 llm_cache = create_directory(".", "llm_cache")
 fine_tune_path = create_directory(llm_cache, "fine-AWQ-INT4")
 
+audio_analysis_save_path = create_directory(".", "analysis")
 auto_audio_classify_path = create_directory(".", "unlabeled-classify")
 label_audio_classify_path = create_directory(".", "classify")
 label_audio_classify_model = create_directory(".", "model", "classify")
+
+multiple_speaker_diarization_model = create_directory(".", "model", "speaker-diarization-3.1")
 
 wav2vec2_model_path = create_directory(".", "model", "wav2vec2-large-robust-12-ft-emotion-msp-dim")
 
--- utils.py
+++ utils.py
@@ -204,3 +204,30 @@ def split_long_wav(path):
         feed_clip.export(os.path.join(config.train_format_audio_path, split_name), format="wav")
 
         split_parts += 1
+
+def get_speaker_dict(path, return_without_save=False):
+    from pyannote.audio import Pipeline
+
+    if config.multiple_speaker_diarization_model.startswith("pyannote"):
+        Pipeline.from_pretrained(config.multiple_speaker_diarization_model, token=config.hugging_face_token)
+    else:
+        Pipeline.from_pretrained(config.multiple_speaker_diarization_model)
+
+    pipeline = pipeline.to(torch.device("cuda"))
+
+    filename = os.path.basename(path)
+    waveform, sample_rate = torchaudio.load(path)
+    diarization = pipeline({"waveform": waveform, "sample_rate": sample_rate})
+
+    split_datas = []
+    for turn, _, speaker in diarization.speaker_diarization.itertracks(yield_label=True):
+        _start_time = int(turn.start * 1000)
+        _stop_time = int(turn.end * 1000)
+
+        if _stop_time > (_start_time + config.min_audio_length_ms):
+            split_datas.append({"s": _start_time, "e": _stop_time, "path": path})
+
+    if not return_without_save:
+        save_json(split_datas, os.path.join(config.audio_analysis_save_path, f"analysis_{filename}_speaker.json"))
+
+    return split_datas
